982c50339d694a31e898a4951d827f5f27bddf02
juliethhurtado <juliethhs@unicauca.edu.co>
2024-05-28 11:08:52 -0500

    Crear proyecto

---
+++ SisInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SisInventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
